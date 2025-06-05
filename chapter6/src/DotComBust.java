dbb4042804f935716d6391d7288c7cb3d3197fd8
Aamer <mohamedaamer867@gmail.com>
2025-06-05 16:59:36 +0200

    Finish startPlaying Method

--- chapter6/src/DotComBust.java
+++ chapter6/src/DotComBust.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 
 public class DotComBust {
-    GameHelper helper;
+    GameHelper helper = new GameHelper();
     ArrayList<DotCom> dotComList = new ArrayList<DotCom>();
     int numGuesses = 0;
+    String[] dotComNames;
 
     public void setUpGame() {
 
@@ -12,16 +13,22 @@ public class DotComBust {
             dotComList.add(dotCom);
         }
 
-        String[] dotComNames = new String[dotComList.size()];
+        dotComNames = new String[dotComList.size()];
 
         dotComNames[0] = "<DE>";
         dotComNames[1] = "<US>";
         dotComNames[2] = "<UK>";
 
-        helper = new GameHelper();
-
         for (DotCom dotCom : dotComList) {
             dotCom.setLocationCells(helper.placeDotCom());
         }
     }
+
+    public void startPlaying() {
+        while(!dotComList.isEmpty()){
+            checkUserGuess(helper.getUserInput());
+        }
+    }
+
+    public void checkUserGuess(String guess) {}
 }
