c1ad1528bd3233b946640ee0f2c9249bf463e22f
Aamer <mohamedaamer867@gmail.com>
2025-06-17 23:21:34 +0200

    just adding some comments for notes

--- chap8/src/Main.java
+++ chap8/src/Main.java
@@ -2,14 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Class method is the class, which can not be extended anymore.
+        // An abstract method is the method, which it must be overridden.
+        // We can't have an abstract method, without an abstract class.
+        // If we have an abstract method within a class, when we extend this class,
+        // we must implement all abstract methods within the extended class.
+        // We need abstract methods only to Polymorphism.
+        // Why to have this concept of the abstract methods??
+        //      To be able to ensure the Polymorphism, like if we need to use the Animal class
+        //      as an array type and loop over this array to and apply a specific method in
+        //      the Animal class, but with every subclass behaviour. Making the method abstract
+        //      will ensure that every subclass has its own method implementation.
+        //      If the subclass is abstract, we can easily ignore to implement it,
+        //      because if the subclass is abstract, we will not be able to instantiate it,
+        //      so we will not be able to call the method. But if we make a subclass that extends the
+        //      abstract subclass, we must implement all the abstract methods in the both:
+        //      the extended abstract class, the abstract superclass. So we need to make them all from the superclass
+        //      till the class, which will be able to be instantiated.
+        //      When implementing the method, we can easily ignore to add Override annotation.
+        // So at the end, we have both types of classes:
+        //      Abstract class, Concrete class.
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
