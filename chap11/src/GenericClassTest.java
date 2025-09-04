4e2b8345a19529910ee073c26922bdcfae6cfdeb
Mohamed Aamer <mo7amed3amer@gmail.com>
2025-09-04 19:28:18 +0200

    First commit PC

---
+++ chap11/src/GenericClassTest.java
@@ -0,0 +1,7 @@
+public class GenericClassTest<T> {
+    public T dog;
+
+    public static void main(String[] args) {
+        GenericClassTest<Animal> genClassTest=new GenericClassTest<>();
+    }
+}
